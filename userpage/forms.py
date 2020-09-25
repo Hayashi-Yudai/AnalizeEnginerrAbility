0be3f66b3d7c6128885bd5d5ccef1ffe9b8e4a7f
Yudai Hayashi <yudai0913haya@gmail.com>
2020-09-25 19:56:15 +0900

    フォームをテスト、フォームのバリデーションを追加した

--- userpage/forms.py
+++ userpage/forms.py
@@ -1,9 +1,16 @@
 from django import forms
+from django.core.exceptions import ValidationError
+
+
+def not_has_space(value: str) -> Exception:
+    if " " in value:
+        raise ValidationError("Do NOT contain any spaces")
 
 
 class AccountSetForm(forms.Form):
     username = forms.CharField(
         max_length=200,
+        validators=[not_has_space],
         widget=forms.TextInput(
             attrs={
                 "class": "form-control",
--- userpage/tests.py
+++ userpage/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from userpage.forms import AccountSetForm
 
-# Create your tests here.
+
+def test_valid_username():
+    testcase = "Hayashi-Yudai"
+    form = AccountSetForm({"username": testcase})
+
+    assert form.is_valid()
+
+
+def test_space_is_invalid():
+    testcase = "Hayashi Yudai"
+    form = AccountSetForm({"username": testcase})
+
+    assert not form.is_valid()
+    assert form.errors["username"][0] == "Do NOT contain any spaces"
