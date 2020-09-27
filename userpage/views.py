36f8349c6955fb9c921d099553e45ffb692cb52e
Yudai Hayashi <yudai0913haya@gmail.com>
2020-09-27 19:12:33 +0900

    つけたスター数からスコアを計算

--- userpage/views.py
+++ userpage/views.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from django.shortcuts import render
 from django.views import View
 import os
@@ -10,7 +11,7 @@ from userpage.forms import AccountSetForm
 
 class Index(View):
     def get(self, request, *args, **kwargs):
-        context = {"form": AccountSetForm(), "total_score": 50}
+        context = {"form": AccountSetForm(), "star_score": 50}
         return render(request, "userpage/index.html", context)
 
     def post(self, request, *args, **kwargs):
@@ -20,12 +21,13 @@ class Index(View):
         if form.is_valid():
             username = data["username"]
             repo_infos = self.get_repositories(username)
+            star_score = self.calc_star_score(username)
 
             context = {
                 "form": form,
                 "username": username,
                 "repo_infos": repo_infos,
-                "total_score": 50,
+                "star_score": star_score,
             }
 
             return render(request, "userpage/index.html", context)
@@ -60,3 +62,44 @@ class Index(View):
                 )
 
         return repo_infos
+
+    def calc_star_score(self, username: str) -> float:
+        auth = HTTPBasicAuth(
+            os.environ.get("API_USERNAME"), os.environ.get("API_TOKEN")
+        )
+
+        user_info = requests.get(
+            f"https://api.github.com/users/{username}", auth=auth
+        ).json()
+
+        account_created_at = datetime.strptime(
+            user_info["created_at"], "%Y-%m-%dT%H:%M:%SZ"
+        )
+        last_updated_at = datetime.strptime(
+            user_info["updated_at"], "%Y-%m-%dT%H:%M:%SZ"
+        )
+
+        elapsed_days = (last_updated_at - account_created_at).days
+
+        star_count = 0
+        page = 1
+
+        while True:
+            star_repositories = requests.get(
+                f"https://api.github.com/users/{username}/starred?per_page=100&page={page}",
+                auth=auth,
+            ).json()
+
+            if len(star_repositories) == 0:
+                break
+
+            page += 1
+            star_count += len(star_repositories)
+
+        bias = 1000 if elapsed_days < 1000 else 0
+        star_per_day_biased = star_count / (elapsed_days + bias)
+
+        deviation_val = (star_per_day_biased - 0.02862) / 0.1257 * 10 + 50
+        deviation_val = int(deviation_val * 100) / 100
+
+        return min(deviation_val, 100)
