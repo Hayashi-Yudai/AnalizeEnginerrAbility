32276331ee0f308ef0ba81820a3cd1be47334cd3
Yudai Hayashi <yudai0913haya@gmail.com>
2020-09-30 14:10:08 +0900

    Pull Request のスコアを実装した

--- userpage/views.py
+++ userpage/views.py
@@ -1,7 +1,6 @@
 from datetime import datetime
 from django.shortcuts import render
 from django.views import View
-import os
 from typing import List, Dict, Any
 
 from userpage.forms import AccountSetForm
@@ -19,6 +18,8 @@ class Index(View):
             "star_score_posi": 40,
             "issue_score": 50.00,
             "issue_score_posi": 40,
+            "pr_score": 50,
+            "pr_score_posi": 40,
         }
 
     def get(self, request, *args, **kwargs):
@@ -38,6 +39,7 @@ class Index(View):
             repo_infos = self.get_repositories(username)
             star_score = self.calc_star_score(username, elapsed_days)
             issue_score = self.calc_issue_score(username, elapsed_days)
+            pull_request_score = self.calc_pull_request_score(username)
 
             context = {
                 "form": form,
@@ -47,6 +49,8 @@ class Index(View):
                 "star_score_posi": star_score - 13.00,
                 "issue_score": issue_score,
                 "issue_score_posi": issue_score - 13.00,
+                "pr_score": pull_request_score,
+                "pr_score_posi": pull_request_score - 13.00,
             }
 
             return render(request, "userpage/index.html", context)
@@ -144,3 +148,54 @@ class Index(View):
         return self.calc_biased_deviation_value(
             issue_count, mean=0.01043, stdev=0.04264, elapsed_days=elapsed_days
         )
+
+    def calc_pull_request_score(self, username: str) -> float:
+        query = (
+            '{ user(login: "'
+            + username
+            + '") {'
+            + """
+            pullRequests(last: 100, orderBy:{direction: DESC, field:CREATED_AT}) {
+                totalCount
+                nodes {
+                    merged
+                    author {
+                        login
+                    }
+                    mergedBy {
+                        login
+                    }
+                }
+            }
+        }
+    }
+            """
+        )
+        response: Dict[str, Any] = self.api.post_graphql(query)["data"]["user"]
+        if response is None:
+            return 0
+
+        pull_request_num: int = response["pullRequests"]["totalCount"]
+        pull_requests: List[Dict[str, Any]] = response["pullRequests"]["nodes"]
+
+        merged_pr_num = 0
+        own_pr_num = 0
+
+        for pr in pull_requests:
+            if pr["merged"]:
+                merged_pr_num += 1
+
+            if (
+                pr["mergedBy"] is not None
+                and pr["author"]["login"] == pr["mergedBy"]["login"]
+            ):
+                own_pr_num += 1
+
+        own_pr_ratio = own_pr_num / merged_pr_num
+        merged_ratio = merged_pr_num / pull_request_num
+        if own_pr_ratio < 0.8:
+            score = self.calc_deviation_value(merged_ratio, mean=0.719, stdev=0.168)
+        else:
+            score = self.calc_deviation_value(merged_ratio, mean=0.869, stdev=0.104)
+
+        return score
