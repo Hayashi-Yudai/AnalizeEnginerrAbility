43f2588cfe4e49f0811602e5168ba02f03162016
Yudai Hayashi <yudai0913haya@gmail.com>
2020-10-02 10:54:53 +0900

    ポップアップ画面の挙動を改善

---
+++ userpage/static/userpage/js/manage_popup.js
@@ -0,0 +1,11 @@
+function popup_appear() {
+    let popup = document.getElementById("popup1");
+    popup.style.visibility = "visible";
+    popup.style.opacity = "1";
+}
+
+function popup_disappear() {
+    let popup = document.getElementById("popup1");
+    popup.style.visibility = "hidden";
+    popup.style.opacity = "0";
+}
